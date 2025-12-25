d2e63ef77184651f1cdbf5375a2fce1fe4bf392b
Subrat-Pradhan <subratkp27@gmail.com>
2025-12-25 23:37:23 +0530

    testcasesAdded

---
+++ cypress-learning/e2e/test1.cy.js
@@ -0,0 +1,44 @@
+describe("Recording 12/25/2025 at 3:39:45 PM", () => {
+  it("tests Recording 12/25/2025 at 3:39:45 PM", () => {
+    cy.viewport(1339, 911);
+    cy.visit("https://demowebshop.tricentis.com/");
+    cy.get("div.header img").click();
+    cy.location("href").should("eq", "https://demowebshop.tricentis.com/");
+    cy.get("#small-searchterms").click();
+    cy.get("#small-searchterms").type("laptop");
+    cy.get("#ui-id-3", { timeout: 10000 }).should("be.visible").click(); // Added timeout for search suggestion
+    cy.location("href").should("eq", "https://demowebshop.tricentis.com/141-inch-laptop");
+    cy.get("#add-to-cart-button-31", { timeout: 10000 }).should("be.visible").click();
+    cy.get("#topcartlink span.cart-label").click();
+    cy.location("href").should("eq", "https://demowebshop.tricentis.com/cart");
+    cy.get("#termsofservice").click();
+    cy.get("#checkout").click();
+    cy.location("href").should("eq", "https://demowebshop.tricentis.com/login/checkoutasguest?returnUrl=%2Fcart");
+    cy.get("input.checkout-as-guest-button", { timeout: 10000 }).should("be.visible").click();
+    cy.location("href").should("eq", "https://demowebshop.tricentis.com/onepagecheckout");
+    cy.get("#BillingNewAddress_FirstName").should("be.visible").click();
+    cy.get("#BillingNewAddress_FirstName").type("Subrat");
+    cy.get("#BillingNewAddress_LastName").type("Pradhan");
+    cy.get("#BillingNewAddress_Email").type("test@test.com");
+    cy.get("#BillingNewAddress_CountryId").select("United States"); // Changed to select for reliability
+    cy.get("#BillingNewAddress_City").type("New York");
+    cy.get("#BillingNewAddress_Address1").type("123 Test St");
+    cy.get("#BillingNewAddress_ZipPostalCode").type("10001");
+    cy.get("#BillingNewAddress_PhoneNumber").type("1234567890");
+
+    cy.get("#billing-buttons-container > input", { timeout: 10000 }).should("be.visible").click();
+
+    // Shipping Address/Method section might take time to expand
+    cy.get("#shipping-buttons-container > input", { timeout: 10000 }).should("be.visible").click();
+
+    cy.get("#payment-method-buttons-container > input", { timeout: 10000 }).should("be.visible").click();
+
+    cy.get("#payment-info-buttons-container > input", { timeout: 10000 }).should("be.visible").click();
+
+    cy.get("#confirm-order-buttons-container > input", { timeout: 10000 }).should("be.visible").click();
+
+    cy.location("href", { timeout: 10000 }).should("eq", "https://demowebshop.tricentis.com/checkout/completed/");
+    cy.get("div.master-wrapper-main input").should("be.visible").click();
+    cy.location("href").should("eq", "https://demowebshop.tricentis.com/");
+  });
+});
